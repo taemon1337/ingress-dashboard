bff3ae9923dd7dd08d394e77965b6bc4d17aab4c
tim <taemon1337@gmail.com>
2022-02-28 08:56:10 -0500

    added curler and helm chart

---
+++ curl/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <output-json-file>" >&2
+  exit 1
+fi
+
+JSONFILE="$1"
+SLEEP="${SLEEP:-3000}"
+APISERVER="${APISERVER:-https://kubernetes.default.svc}"
+SERVICEACCOUNT="${SERVICEACCOUNT:-/var/run/secrets/kubernetes.io/serviceaccount}"
+ENDPOINT="${ENDPOINT:-api/networking.k8s.io/v1/ingresses}"
+NAMESPACE=$(cat ${SERVICEACCOUNT}/namespace)
+TOKEN=$(cat ${SERVICEACCOUNT}/token)
+CACERT=${SERVICEACCOUNT}/ca.crt
+
+while true; do
+  if curl --fail -o "${JSONFILE}" --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/${ENDPOINT} ; then
+    echo "[SUCCESS] $(date) Downloaded ${ENDPOINT} to ${JSONFILE}"
+  else
+    echo "[FAIL] $(date) Error downloading ${ENDPOINT}"
+  fi
+  sleep "${SLEEP}"
+done
