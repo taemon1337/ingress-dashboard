be3e0b2b953d08c5372b2fcf182df153f707d84e
tim <taemon1337@gmail.com>
2022-03-04 11:33:31 -0500

    back to static site

--- ui/src/environment.js
+++ ui/src/environment.js
@@ -1,6 +1,6 @@
 export const environment = () => {
   return {
-    IMAGE_API: import.meta.env.VITE_IMAGE_API || "http://images.svc.cluster.local:8080",
+    IMAGE_API: import.meta.env.VITE_IMAGE_API || "/",
     IMAGE_HEIGHT: import.meta.env.VITE_IMAGE_HEIGHT || 32,
     CARD_CLASS: (import.meta.env.VITE_CARD_CLASS || "grid_md:grid-cols-4_sm:grid-cols-3_xs:grid-cols-1_gap-4").replaceAll('_', ' '),
   }
